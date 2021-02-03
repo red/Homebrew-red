03858522b89f6f23159c3230451edb0563f0346b
Christopher Ross-Gill <chris.rg@ross-gill.com>
2021-02-03 11:33:25 -0500

    Update red-latest.rb
    Update reference to HTTPS location

--- Casks/red-latest.rb
+++ Casks/red-latest.rb
@@ -2,7 +2,7 @@ cask 'red-latest' do
 version :latest
 sha256 :no_check
 
- url "http://static.red-lang.org/dl/auto/mac/red-latest"
+ url "https://static.red-lang.org/dl/auto/mac/red-latest"
  name 'Red Programming Language'
  homepage 'http://www.red-lang.org'
 
